8071a7a011001f8a17d35765011c7f01d2a80044
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-16 16:58:53 -0400

    finished the merge method did not test

--- Merge.java
+++ Merge.java
@@ -1,34 +1,39 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 public class Merge{
   public static void merge(int[] data){
   mergesort(data, 0, data.length -1);}
 
   private static void mergesort(int[] data, int lo, int hi){
-    if(hi > lo){
+    if(hi - lo > 1){
       mergesort(data, (lo + hi) / 2, hi);
       mergesort(data, lo, (lo + hi) / 2);}
+    System.out.println(Arrays.toString(data));
     merge(data, lo, hi);}
 
-  private static void merge(int[] data, int lo, int hi){
-
+  public static void merge(int[] data, int lo, int hi){
     ArrayList<Integer> dummy = new ArrayList<Integer>();
     ArrayList<Integer> dummy2 = new ArrayList<Integer>();
-    for(int i = lo; i <= hi; i++){
-      dummy.add(data[i]);}
-    for(int i = hi; i < data.length; i++){
-      dummy2.add(data[i]);}
-    int counter = 0;
-    while(dummy2.size() != 0){
-      if(dummy.size() == counter){
-        dummy.add(dummy.remove(0));
-      }
-      else if(dummy.get(counter) > dummy2.get(0)){
-        dummy.add(counter, dummy2.get(0));
-        dummy2.remove(0);}
+    for(int i = lo + 1; i <= hi; i++){
+      dummy.add(i - lo - 1,data[i]);}
+    for(int i = 0; i <= lo; i++){
+      dummy2.add(i, data[i]);}
+    int index = 0;
+    for(int i = 0; i < dummy.size(); i++){
+      if(dummy.get(index) > dummy2.get(i)){
+        while(index < dummy.size() && dummy2.get(i) > dummy.get(index)){
+          index ++;}
+        if(index == dummy.size()){
+          while(i < dummy.size()){
+            dummy2.add(dummy.get(i));
+            i ++;}
+          break;}
+        dummy2.add(i + 1, dummy.get(index));
+        i ++;}
       else{
-        dummy.add(counter + 1, dummy2.get(0));
-        dummy2.remove(1);}}
-    for(int i = lo; i <= hi; i++){
-      data[i] = dummy.get(i);}
-    }
-}
+        dummy2.add(i, dummy.get(index));}
+      index++;}
+    for(int i = 0; i < dummy2.size(); i++){
+      data[i] = dummy2.get(i);
+    }}
+  }
