c19854f1fda4ad5bffdb73228ff0c4b56bcc0788
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-17 12:36:03 -0400

    I can't make it faster without Mr.K's optimization (compared with built in quick sort)
    Size		Max Value	quick/builtin ratio 
    31250		1000000000	3.75
    62500		1000000000	3.909090909090909
    125000		1000000000	4.869565217391305
    250000		1000000000	4.25
    500000		1000000000	3.5643939393939394
    1000000		1000000000	4.3244897959183675
    2000000		1000000000	4.243538767395626
    
    31250		500	2.0
    62500		500	3.9130434782608696
    125000		500	6.642857142857143
    250000		500	6.925925925925926
    500000		500	7.018867924528302
    1000000		500	7.47926267281106
    2000000		500	7.517401392111369
    
    31250		10	15.5
    62500		10	11.833333333333334
    125000		10	15.666666666666666
    250000		10	14.894736842105264
    500000		10	14.564102564102564
    1000000		10	16.173333333333332
    2000000		10	16.233766233766232

--- Merge.java
+++ Merge.java
@@ -13,22 +13,18 @@ public class Merge{
   private static void merge(int[] data, int lo, int hi){
     ArrayList<Integer> dummy = new ArrayList<Integer>((lo + hi) / 2 - lo + 1);
     ArrayList<Integer> dummy2 = new ArrayList<Integer>(hi - (lo + hi) / 2 + 1);
-    for(int i = lo; i <= (lo + hi) / 2; i++){
-      dummy.add(data[i]);}
-    for(int i = (lo + hi) / 2 + 1; i <= hi; i++){
-      dummy2.add(data[i]);}
+    for(int i = lo; i <= (lo + hi) / 2;){
+      dummy.add(data[i++]);}
+    for(int i = (lo + hi) / 2 + 1; i <= hi;){
+      dummy2.add(data[i++]);}
     int k = 0;
     int l = 0;
     while(k < dummy.size() && l < dummy2.size()){
       if(dummy.get(k) <= dummy2.get(l)){
-        data[k + l + lo] = dummy.get(k);
-        k++;}
+        data[k + l + lo] = dummy.get(k++);}
       else{
-        data[k + l + lo] = dummy2.get(l);
-        l++;}}
+        data[k + l + lo] = dummy2.get(l++);}}
     while(k < dummy.size()){
-      data[k + l + lo] = dummy.get(k);
-      k++;}
+      data[k + l + lo] = dummy.get(k++);}
     while(l < dummy2.size()){
-      data[k + l + lo] = dummy2.get(l);
-      l++;}}}
+      data[k + l + lo] = dummy2.get(l++);}}}
