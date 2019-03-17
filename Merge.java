3d6d7d71eff997527a1ae0c2677a29f19d28dda1
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-17 09:21:34 -0400

    finished mergesort and tested using Mr.K's driver

--- Merge.java
+++ Merge.java
@@ -1,39 +1,44 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 public class Merge{
-  public static void merge(int[] data){
-  mergesort(data, 0, data.length -1);}
+  public static void mergesort(int[] data){
+    ArrayList<Integer> bat = new ArrayList<Integer>(data.length);
+    for(int i = 0; i < data.length; i++){
+      bat.add(data[i]);}
+    mergesort(bat, 0, data.length -1);
+    for(int i = 0; i < data.length; i++){
+      data[i] = bat.get(i);}}
 
-  private static void mergesort(int[] data, int lo, int hi){
-    if(hi - lo > 1){
-      mergesort(data, (lo + hi) / 2, hi);
-      mergesort(data, lo, (lo + hi) / 2);}
-    System.out.println(Arrays.toString(data));
-    merge(data, lo, hi);}
+  private static void mergesort(ArrayList<Integer> data, int lo, int hi){
+    if(hi > lo){
+      mergesort(data, lo, (lo + hi) / 2);
+      mergesort(data, (lo + hi) / 2 + 1, hi);
+      merge(data, lo, hi);}}
 
-  public static void merge(int[] data, int lo, int hi){
+  private static void merge(ArrayList<Integer> data, int lo, int hi){
     ArrayList<Integer> dummy = new ArrayList<Integer>();
     ArrayList<Integer> dummy2 = new ArrayList<Integer>();
-    for(int i = lo + 1; i <= hi; i++){
-      dummy.add(i - lo - 1,data[i]);}
-    for(int i = 0; i <= lo; i++){
-      dummy2.add(i, data[i]);}
-    int index = 0;
-    for(int i = 0; i < dummy.size(); i++){
-      if(dummy.get(index) > dummy2.get(i)){
-        while(index < dummy.size() && dummy2.get(i) > dummy.get(index)){
-          index ++;}
-        if(index == dummy.size()){
-          while(i < dummy.size()){
-            dummy2.add(dummy.get(i));
-            i ++;}
-          break;}
-        dummy2.add(i + 1, dummy.get(index));
-        i ++;}
+    for(int i = lo; i <= (lo + hi) / 2; i++){
+      dummy.add(data.get(i));}
+    for(int i = (lo + hi) / 2 + 1; i <= hi; i++){
+      dummy2.add(data.get(i));}
+    int index = lo;
+    int k = 0;
+    int l = 0;
+    while(k < dummy.size() && l < dummy2.size()){
+      if(dummy.get(k) <= dummy2.get(l)){
+        data.set(index, dummy.get(k));
+        index ++;
+        k++;}
       else{
-        dummy2.add(i, dummy.get(index));}
-      index++;}
-    for(int i = 0; i < dummy2.size(); i++){
-      data[i] = dummy2.get(i);
-    }}
-  }
+        data.set(index,dummy2.get(l));
+        index ++;
+        l++;}}
+    while(k < dummy.size()){
+      data.set(index, dummy.get(k));
+      index ++;
+      k++;}
+    while(l < dummy2.size()){
+      data.set(index, dummy2.get(l));
+      index ++;
+      l++;}}}
