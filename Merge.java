a87d2b7361062e1be2ad8480fdc684c6bbc69a25
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-18 06:32:13 -0400

    tested using Ruoshi Mao's driver
    Size		Performance
    31250		4.083
    62500		4.045
    125000		4.024
    250000		4.029
    500000		3.870
    1000000		0.106
    
    Average		3.360

--- Merge.java
+++ Merge.java
@@ -1,30 +1,25 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+
 public class Merge{
   public static void mergesort(int[] data){
     mergesort(data, 0, data.length -1);}
 
   private static void mergesort(int[] data, int lo, int hi){
-    if(hi > lo){
-      mergesort(data, lo, (lo + hi) / 2);
-      mergesort(data, (lo + hi) / 2 + 1, hi);
-      merge(data, lo, hi);}}
+    if(lo >= hi){
+      return;}
+    mergesort(data, lo, (lo + hi) / 2);
+    mergesort(data, (lo + hi) / 2 + 1, hi);
+    merge(data, lo, hi);}
 
   private static void merge(int[] data, int lo, int hi){
     ArrayList<Integer> dummy = new ArrayList<Integer>((lo + hi) / 2 - lo + 1);
     ArrayList<Integer> dummy2 = new ArrayList<Integer>(hi - (lo + hi) / 2 + 1);
-    for(int i = lo; i <= (lo + hi) / 2;){
-      dummy.add(data[i++]);}
-    for(int i = (lo + hi) / 2 + 1; i <= hi;){
-      dummy2.add(data[i++]);}
+    for(int i = lo; i <= (lo + hi) / 2;) {dummy.add(data[i++]);}
+    for(int i = (lo + hi) / 2 + 1; i <= hi;) {dummy2.add(data[i++]);}
     int k = 0;
     int l = 0;
     while(k < dummy.size() && l < dummy2.size()){
-      if(dummy.get(k) <= dummy2.get(l)){
-        data[k + l + lo] = dummy.get(k++);}
-      else{
-        data[k + l + lo] = dummy2.get(l++);}}
-    while(k < dummy.size()){
-      data[k + l + lo] = dummy.get(k++);}
-    while(l < dummy2.size()){
-      data[k + l + lo] = dummy2.get(l++);}}}
+      if(dummy.get(k) <= dummy2.get(l)) {data[k + l + lo] = dummy.get(k++);}
+      else{data[k + l + lo] = dummy2.get(l++);}}
+    while(k < dummy.size()){data[k + l + lo] = dummy.get(k++);}
+    while(l < dummy2.size()){data[k + l + lo] = dummy2.get(l++);}}}
