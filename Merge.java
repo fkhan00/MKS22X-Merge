66e119be978df04dffc9955fba3f32c53d3ad8ca
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-19 11:26:09 -0400

    added insertion sort optimization

--- Merge.java
+++ Merge.java
@@ -5,6 +5,9 @@ public class Merge{
     mergesort(data, 0, data.length -1);}
 
   private static void mergesort(int[] data, int lo, int hi){
+    if(hi - lo == 64){
+      insertionSort(data, lo, hi);
+      return;}
     if(lo >= hi){
       return;}
     mergesort(data, lo, (lo + hi) / 2);
@@ -22,4 +25,21 @@ public class Merge{
       if(dummy.get(k) <= dummy2.get(l)) {data[k + l + lo] = dummy.get(k++);}
       else{data[k + l + lo] = dummy2.get(l++);}}
     while(k < dummy.size()){data[k + l + lo] = dummy.get(k++);}
-    while(l < dummy2.size()){data[k + l + lo] = dummy2.get(l++);}}}
+    while(l < dummy2.size()){data[k + l + lo] = dummy2.get(l++);}}
+
+    private static void insertionSort(int[] ary, int lo, int hi)
+     {
+       int c = 0;
+       int place = 0;
+       for(int i = lo; i <= hi; i++)
+       {
+         place = ary[i];
+         c = i - 1;
+         while(c >= 0 && place <= ary[c])
+         {
+           ary[c + 1] = ary[c];
+           c--;
+         }
+         ary[c + 1] = place;
+       }
+   }}
