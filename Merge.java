34ed01f8970e495f26040d3b4b9c2066c90e3748
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-25 21:14:01 -0400

    fixed memory loss on merge
    I was using array lists in my merge method and because I didn't initialize the lists with their necessary sizes, I ended up calling on the resize function multiple times which lead to significant memory usage for large values. Now, it's much faster and at worst finishes 8 times slower than the built in quick sort.

--- Merge.java
+++ Merge.java
@@ -1,45 +1,44 @@
-import java.util.ArrayList;
-
 public class Merge{
-  public static void mergesort(int[] data){
-    mergesort(data, 0, data.length -1);}
+    public static void merge(int ary[], int lo, int hi){
+        int[] shorts  = new int [(lo + hi) / 2 - lo + 1];
+        int[] tall = new int [hi - (lo + hi) / 2];
+        for (int i=0; i< shorts.length; ++i)
+            shorts[i] = ary[lo + i];
+        for (int j=0; j< tall.length; ++j)
+            tall[j] = ary[(lo + hi) / 2 + 1+ j];
+        int i = 0, j = 0;
+        int k = lo;
+        while (i < shorts.length && j < tall.length)
+        {
+            if (shorts[i] <= tall[j]){
+                ary[k++] = shorts[i++];}
+            else{
+                ary[k ++] = tall[j++];}}
+        while (i < shorts.length){
+            ary[k ++] = shorts[i++];}
+        while (j < tall.length){
+            ary[k ++] = tall[j++];}}
 
-  private static void mergesort(int[] data, int lo, int hi){
-    if(hi - lo == 64 ){
-      insertionSort(data, lo, hi);
+    public static void mergesort(int arr[], int lo, int hi){
+      if(hi - lo == 32){
+        insertionSort(arr, lo, hi);
         return;}
-    if(lo >= hi){
-      return;}
-    mergesort(data, lo, (lo + hi) / 2);
-    mergesort(data, (lo + hi) / 2 + 1, hi);
-    merge(data, lo, hi);}
+        if (hi > lo){
+            mergesort(arr, lo, (lo + hi) / 2);
+            mergesort(arr , (lo + hi) / 2 +1, hi);
+            merge(arr, lo, hi);}}
 
-  private static void merge(int[] data, int lo, int hi){
-    ArrayList<Integer> dummy = new ArrayList<Integer>((lo + hi) / 2 - lo + 1);
-    ArrayList<Integer> dummy2 = new ArrayList<Integer>(hi - (lo + hi) / 2 + 1);
-    for(int i = lo; i <= (lo + hi) / 2;) {dummy.add(data[i++]);}
-    for(int i = (lo + hi) / 2 + 1; i <= hi;) {dummy2.add(data[i++]);}
-    int k = 0;
-    int l = 0;
-    while(k < dummy.size() && l < dummy2.size()){
-      if(dummy.get(k) <= dummy2.get(l)) {data[k + l + lo] = dummy.get(k++);}
-      else{data[k + l + lo] = dummy2.get(l++);}}
-    while(k < dummy.size()){data[k + l + lo] = dummy.get(k++);}
-    while(l < dummy2.size()){data[k + l + lo] = dummy2.get(l++);}}
+    public static void mergesort(int[] ary){
+      mergesort(ary, 0, ary.length - 1);}
 
-    private static void insertionSort(int[] ary, int lo, int hi)
-     {
+    private static void insertionSort(int[] ary, int lo, int hi){
        int c = 0;
        int place = 0;
-       for(int i = lo; i <= hi; i++)
-       {
+       for(int i = lo; i <= hi; i++){
          place = ary[i];
          c = i - 1;
-         while(c >= 0 && place <= ary[c])
-         {
+         while(c >= 0 && place <= ary[c]){
            ary[c + 1] = ary[c];
-           c--;
-         }
-         ary[c + 1] = place;
-       }
-   }}
+           c--;}
+         ary[c + 1] = place;}}
+}
